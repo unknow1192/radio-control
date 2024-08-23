c590c7a9f57164fb939c2528e6dda7894583ab94
Stanislaw Laskowski <stanislawlask097@gmail.com>
2024-08-23 13:38:21 +0200

    add stop playing station and some css

--- app.py
+++ app.py
@@ -3,17 +3,18 @@ import subprocess
 
 app = Flask(__name__)
 
-stations = {"Nowy swiat" : "https://stream.open.fm/368?type=.aac&user=800043471858&player_group=WWW"}
+stations = {"Nowy swiat": "https://stream.open.fm/368?type=.aac&user=800043471858&player_group=WWW"}
 
 current_process = None
+current_station = None
 
 @app.route('/')
 def index():
-    return render_template('index.html', stations = stations)
+    return render_template('index.html', stations=stations, current_station=current_station)
 
-@app.route('/play', methods = ['POST'])
+@app.route('/play', methods=['POST'])
 def play():
-    global current_process
+    global current_process, current_station
 
     station = request.form.get('station')
     url = stations.get(station)
@@ -22,18 +23,20 @@ def play():
         current_process.terminate()
 
     current_process = subprocess.Popen(['cvlc', '--no-video', url], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    current_station = station
 
-    return f"Playing {station}"
+    return render_template('index.html', stations=stations, current_station=current_station)
 
-@app.route('/stop', methods = ['POST'])
+@app.route('/stop', methods=['POST'])
 def stop():
-    global current_process
+    global current_process, current_station
 
     if current_process:
         current_process.terminate()
         current_process = None
+        current_station = None
 
-    return "Stopped playing"
+    return render_template('index.html', stations=stations, current_station=current_station)
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', port = 8080)
+    app.run(host='0.0.0.0', port=8080)
