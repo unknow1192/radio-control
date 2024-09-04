ba34681bc0e8e85bdf5e5b2c7cf78aeaf408b613
Stanislaw Laskowski <stanislawlask097@gmail.com>
2024-09-04 18:31:28 +0200

    prepare to push

--- stations.py
+++ stations.py
@@ -1,6 +1,4 @@
 stations = {
-    "Nowy swiat": "https://stream.open.fm/368?type=.aac&user=800043471858&player_group=WWW",
-    "Radio 357": "http://193.222.135.71/378",
-    #"Trójka": "http://mp3.polskieradio.pl:8904/;.mp3",
-    "Radio Zet": "http://zet-net-01.cdn.eurozet.pl:8400/"
+    "Example station": "https://example.station",
+    "Example station 2": "https://example.station/station2"
 }
